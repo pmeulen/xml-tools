5ea6941fae4de857e576f319c8f7fc0963def4db
Pieter van der Meulen <pieter.vandermeulen@surf.nl>
2025-03-22 11:18:29 +0100

    - Follow HTTP Redirects when downloading schema's - Make the schema DB included in the script more readable - Improve bash style used in the script and make things a bit more readable in general

--- README.md
+++ README.md
@@ -1,25 +1,28 @@
 xml-tools
 =========
 
-Scripts to ease working with XML schemas on OSX and Linux. Works out-of-the box on OSX 
-with limxml2-utils from macports.
+Scripts to make working with XML schemas simpler on OSX and Linux by generating an XML Catalog. 
+The scripts works out-of-the box on OSX with `limxml2-utils` from [macports](https://www.macports.org/).
+It should be easy to adapt to other systems by changing the paths in `create_catalog.sh`.
 
 # Creating an XML Catalog using create_catalog.sh 
 
-create_catalog.sh downloads a bunch of schema's and creates an local XML catalog from them.
-It requires xmlcatalog and curl. xmlcatalog is typicially packaged in libxml2-utils.
-The generated catalog is used by xmllib2 and xmllint will automatically
-when it is stored in a well known location. For a Linux distro, you probrally need 
-to update the `CATALOG_LOCATION` and `CATALOG_SHARE` in `create_catalog.sh` first.
-You can use "xmllint --help" to find out what this location is on your system. Other
-applications (like the Oxygen XML Editor on OSX) can use the catalog as well.
+`create_catalog.sh` downloads a bunch of schema's and creates an local XML catalog from them.
+It requires `xmlcatalog` and `curl`. `xmlcatalog` is typically packaged in `libxml2-utils`.
+The generated catalog is used by `xmllib2` and `xmllint` will automatically be used
+if it is stored in a well known location. 
 
-Schema's are downloaded once and stored locally. See create_catalog.sh for the list 
-of downloaded schema's and paths.
+Schema's are downloaded once and stored locally. See `create_catalog.sh` for the list
+of downloaded schema's and paths, and update it to your needs.
+
+## Installation on other systems
+For a Linux distro, you probably need to update the `CATALOG_LOCATION` and `CATALOG_SHARE` 
+in `create_catalog.sh` first so the catalog is stored in a location that is used by `xmllint`.
+You can use `xmllint --help` to find out what this location is on your system. 
 
 To create the catalog or to update an existing catalog run: `create_catalog.sh`
 
-## Using the created catalog in Oxygen XML Editor
+# Using the created catalog in Oxygen XML Editor
 
 In the "Oxygen XML Editor" preferences go to XML->XML Schema and under "catalogs" add: 
 `file:/opt/local/etc/xml/catalog`
@@ -27,9 +30,9 @@ In the "Oxygen XML Editor" preferences go to XML->XML Schema and under "catalogs
 Now XML schema validation and "Show definition" works for XML files that use schema's
 that are defined in the catalog.
 
-# xsdvalidate
+# Using the included `xsdvalidate` script
 
-Validates an XML file against it's schema using the XML catalog to
-lookup the schema's used in the XML file by namespace. Requires xmllint.
+`xsdvalidate` validates an XML file against it's schema using the XML catalog to
+lookup the schema's used in the XML file by namespace. Requires `xmllint`.
 
 Use: `xsdvalidate <XML file>`
--- create_catalog.sh
+++ create_catalog.sh
@@ -1,47 +1,56 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
 
 # @copyright  Copyright 2013 SURFnet bv, The Netherlands
 # @author     Pieter van der Meulen <Pieter.vanderMeulen@surfnet.nl>
 # @license    http://www.apache.org/licenses/LICENSE-2.0
 
-
-# Create a catalog (for use with libxml2) of downloaded schema's
-# When the catalog file already exists new schema's are added to the catalog
+# Create a catalog (for use with libxml2) of downloaded schemas
+# When the catalog file already exists new schemas are added to the catalog
 # It is safe to run the script multiple times, or to abort it.
 
 # Specify where to store the catalog using the CATALOG_LOCATION below
 # xmllib2 and xmllint (from libxml2-utils) will use the catalog automatically
 # when it is stored in a well known location. Use "xmllint --help" to see
-# what this location is on your system
-# Other tools (e.g. Oxygen XML Editor) can use the catalog as well
+# what this location is on your system.
+# Other tools (e.g. Oxygen XML Editor) can use the catalog as well, but you
+# typically have to configure the location of the catalog in the tool.
 
 # Set CATALOG_SHARE below to the directory to store the downloaded schema's.
-# The schema's are referenced in the catalog to that location.
+# The schemas are referenced in the catalog to that location.
 
-# Requires xmlcatalog and xmllint. These are typicially packaged in
+# Requires xmlcatalog and xmllint. These are typically packaged in
 # libxml2-utils
 
+# Debian, Redhat, ...
+#CATALOG_LOCATION=/etc/xml/catalog            # Location of the catalog file
+#CATALOG_SHARE=/usr/local/share/xml           # Directory to store the (downloaded) XML schemas
 
-#Debian, Redhat, ...
-#CATALOG_LOCATION=/etc/xml/catalog
-#CATALOG_SHARE=/usr/local/share/xml
-
-#OSX port
-CATALOG_LOCATION=/opt/local/etc/xml/catalog
-CATALOG_SHARE=/opt/local/share/xml
+# OSX macports.org
+CATALOG_LOCATION=/opt/local/etc/xml/catalog   # Location of the catalog file
+CATALOG_SHARE=/opt/local/share/xml            # Directory to store the (downloaded) XML schemas
 
 #LOCAL_FILE_PREFIX=""
 LOCAL_FILE_PREFIX="file://"
 
-CWD=`pwd`
-cd `dirname $0`
-LOCAL_SCHEMA_DIR="file://"`pwd`"/schemas"
-cd ${CWD}
+# Set the location of LOCAL_SCHEMA_DIR to the schemas directory relative to this script's location
+CWD=$(pwd)
+cd "$(dirname $0)"
+LOCAL_SCHEMA_DIR="file://$(pwd)/schemas"
+cd "${CWD}"
 
 # Schemas to add to the catalog. Format:
 # <directory in CATALOG_SHARE to store schema>;<Schema download URL>;<Filename for schema on disk>
+# lines starting with # are ignored
 
-SCHEMA_DB="saml;http://docs.oasis-open.org/security/saml/v2.0/saml-schema-assertion-2.0.xsd;saml-schema-assertion-2.0.xsd
+SCHEMA_DB=$(cat <<EOF
+# XML Schema
+xml;http://www.w3.org/2001/xml.xsd;xml.xsd
+
+# SAML 2.0 schema's from OASIS
+saml;http://docs.oasis-open.org/security/saml/v2.0/saml-schema-assertion-2.0.xsd;saml-schema-assertion-2.0.xsd
 saml;http://docs.oasis-open.org/security/saml/v2.0/saml-schema-authn-context-2.0.xsd;saml-schema-authn-context-2.0.xsd
 saml;http://docs.oasis-open.org/security/saml/v2.0/saml-schema-authn-context-auth-telephony-2.0.xsd;saml-schema-authn-context-auth-telephony-2.0.xsd
 saml;http://docs.oasis-open.org/security/saml/v2.0/saml-schema-authn-context-ip-2.0.xsd;saml-schema-authn-context-ip-2.0.xsd
@@ -76,85 +85,120 @@ saml;http://docs.oasis-open.org/security/saml/v2.0/saml-schema-x500-2.0.xsd;saml
 saml;http://docs.oasis-open.org/security/saml/v2.0/saml-schema-xacml-2.0.xsd;saml-schema-xacml-2.0.xsd
 saml;http://docs.oasis-open.org/security/saml/Post2.0/sstc-saml-metadata-ui/v1.0/cs01/xsd/sstc-saml-metadata-ui-v1.0.xsd;sstc-saml-metadata-ui-v1.0.xsd
 saml;${LOCAL_SCHEMA_DIR}/shibmd-1.0.xsd;shibmd-1.0.xsd
+
+# SOAP schema's
 soap;http://schemas.xmlsoap.org/soap/envelope/;soap-envelope-1.1.xsd
 soap;http://www.w3.org/2003/05/soap-envelope/;soap-envelope-1.2.xsd
+
+# XML-security schema's
 xmlsec;http://www.w3.org/TR/xmldsig-core/xmldsig-core-schema.xsd;xmldsig-core-schema.xsd
 xmlsec;http://www.w3.org/TR/2002/REC-xmldsig-core-20020212/xmldsig-core-schema.xsd;xmldsig-core-schema.xsd
 xmlsec;http://www.w3.org/TR/xmlenc-core/xenc-schema.xsd;xenc-schema.xsd
 xmlsec;http://www.w3.org/TR/2002/REC-xmlenc-core-20021210/xenc-schema.xsd;xenc-schema.xsd
-xml;http://www.w3.org/2001/xml.xsd;xml.xsd
-gpx;http://www.topografix.com/gpx/1/1/gpx.xsd;gpx-1.1.xsd
+
+# XACML schema's
 xacml;http://docs.oasis-open.org/xacml/2.0/access_control-xacml-2.0-context-schema-os.xsd;access_control-xacml-2.0-context-schema-os.xsd
 xacml;http://docs.oasis-open.org/xacml/2.0/access_control-xacml-2.0-policy-schema-os.xsd;access_control-xacml-2.0-policy-schema-os.xsd
 xacml;http://docs.oasis-open.org/xacml/2.0/access_control-xacml-2.0-saml-assertion-schema-os.xsd;access_control-xacml-2.0-saml-assertion-schema-os.xsd
 xacml;http://docs.oasis-open.org/xacml/2.0/access_control-xacml-2.0-saml-protocol-schema-os.xsd;access_control-xacml-2.0-saml-protocol-schema-os.xsd
 xacml;http://docs.oasis-open.org/xacml/3.0/xacml-core-v3-schema-wd-17.xsd;xacml-core-v3-schema-wd-17.xsd
+
+# Garmin GPX schema
+gpx;http://www.topografix.com/gpx/1/1/gpx.xsd;gpx-1.1.xsd
+
+# Dutch eHerkenning schema's
 eherkenning;${LOCAL_SCHEMA_DIR}/etoegang-1.10-service-catalog.xsd;etoegang-1.10-service-catalog.xsd
-eherkenning;${LOCAL_SCHEMA_DIR}/etoegang-1.9-metadata-extension.xsd;etoegang-1.9-metadata-extension.xsd"
+eherkenning;${LOCAL_SCHEMA_DIR}/etoegang-1.9-metadata-extension.xsd;etoegang-1.9-metadata-extension.xsd
+
+# Add new schema's here
+EOF
+)
+
 
 ########################
+# Main script starts here
+# Check for required commands
 
-# Exit on error
-set -e
+for cmd in xmlcatalog xmllint curl; do
+    if ! command -v $cmd &> /dev/null; then
+        echo "$cmd could not be found"
+        exit 1
+    fi
+done
 
-
-if [ -d $CATALOG_SHARE ]
-then
+if [[ -d $CATALOG_SHARE ]]; then
     echo "$CATALOG_SHARE exists"
 else
     echo "$CATALOG_SHARE does not exists or is not a directory"
     exit 1
 fi
 
-if [ -f $CATALOG_LOCATION ]
-then
+if [[ -f $CATALOG_LOCATION ]]; then
     echo "$CATALOG_LOCATION exists"
 else
 	echo "Creating catalog in $CATALOG_LOCATION"
 	xmlcatalog --noout --create $CATALOG_LOCATION
 fi
 
-for line in $SCHEMA_DB
-do
-    dir=`echo $line | cut -d';' -f1`
-    url=`echo $line | cut -d';' -f2`
-    filename=`echo $line | cut -d';' -f3`
+# Process SCHEMA_DB
+while IFS= read -r line; do
+    # Skip lines that start with a #
+    if [[ $line == \#* ]]; then
+        continue
+    fi
+
+    # Skip empty lines
+    if [ -z "$line" ]; then
+        continue
+    fi
+
+    dir=$(echo $line | cut -d';' -f1)
+    url=$(echo $line | cut -d';' -f2)
+    filename=$(echo $line | cut -d';' -f3)
 
     schema_location=$CATALOG_SHARE/$dir/$filename
 
-    # When schema does not exists, download it
-    if [ ! -f $schema_location ]
+    # When a schema file does not exist, download it
+    if [[ ! -f $schema_location ]]
     then
         # Create subdir for schema when required
-        if [ ! -e $CATALOG_SHARE/$dir ]
+        if [[ ! -e $CATALOG_SHARE/$dir ]]
         then
             echo "Creating directory $CATALOG_SHARE/$dir"
-            mkdir $CATALOG_SHARE/$dir
+            mkdir -p "$CATALOG_SHARE/$dir"
         fi
 
-        # Download the schema to a temp file
+        # Download the schema to a temp file first
         echo "Downloading $url:"
-        temp_basename=`basename $0`
-        tempfile=`mktemp /tmp/${temp_basename}.XXXXXX`
-        curl -# $url > $tempfile
+        temp_basename=$(basename $0)
+        tempfile=$(mktemp /tmp/${temp_basename}.XXXXXX)
+        # Download schema
+        # -L follow redirects
+        # -# show progress
+        curl -L -# "$url" > "$tempfile"
 
-        # Move schema in place
-        mv $tempfile $CATALOG_SHARE/$dir/$filename
+        # Then move schema in place
+        mv "$tempfile" $CATALOG_SHARE/$dir/$filename
     fi
 
     # Add schema to the catalog using download uri
     echo "Adding schemaLocation: <uri name=\"$url\" uri=\"${LOCAL_FILE_PREFIX}$schema_location\"/>"
-    xmlcatalog --noout --add uri $url ${LOCAL_FILE_PREFIX}$schema_location $CATALOG_LOCATION
+    echo "xmlcatalog --noout --add uri \"$url\" ${LOCAL_FILE_PREFIX}$schema_location $CATALOG_LOCATION"
+    xmlcatalog --noout --add uri "$url" ${LOCAL_FILE_PREFIX}$schema_location $CATALOG_LOCATION
 
     # Get target namespace from the schema
-    target_namespace=`xmllint $schema_location --xpath "string(/child::node()/@targetNamespace)"`
+    target_namespace=$(xmllint "$schema_location" --xpath "string(/child::node()/@targetNamespace)")
 
-    if [ ! -z "$target_namespace" ]
-    then
+    if [[ ! -z "$target_namespace" ]]; then
         # Add schema to the catalog by target namespace
         echo "Adding targetNamespace: <uri name=\"$target_namespace\" uri=\"${LOCAL_FILE_PREFIX}$schema_location\"/>"
-        xmlcatalog --noout --add uri $target_namespace ${LOCAL_FILE_PREFIX}$schema_location $CATALOG_LOCATION
+        xmlcatalog --noout --add uri "$target_namespace" ${LOCAL_FILE_PREFIX}"$schema_location" $CATALOG_LOCATION
     fi
-done
+done <<< "$SCHEMA_DB"
 
-exit
+# Done
+echo "Done"
+echo "Catalog file is $CATALOG_LOCATION"
+echo "Schemas are stored in $CATALOG_SHARE"
+
+exit 0
